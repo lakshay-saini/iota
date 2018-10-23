a50288d81ef0a504c05d6dfbfde0f07323fcb744
lakshay <lakshay.laky@gmail.com>
2018-10-23 19:32:14 +0530

    seedUtil complete

--- src/main/java/com/example/iota/SeedUtils.java
+++ src/main/java/com/example/iota/SeedUtils.java
@@ -2,127 +2,58 @@ package com.example.iota;
 
 import org.apache.commons.codec.binary.Base64;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.security.DigestException;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.util.Arrays;
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
 
 public class SeedUtils {
 
-    static byte [] deriveKeyAndIV(byte[] password, byte[] salt) throws
-            DigestException, NoSuchAlgorithmException {
+    public static void main(String args[]) throws Exception {
 
-        byte[] res = new byte[48];
+        String value = "somerandstr12345";
 
-        final MessageDigest md5 = MessageDigest.getInstance("MD5");
+        String PhpEncodedString = "9ud86wfgllkbiJ0VBcwrhAvnWnBB8eBoTjTMiaMlOKQ=";
+        String javaEncodedString = "lB4zuOZyA2LWJ6RYyca5Ghx0HYd6psDb27H51DLKQAc=";
 
-        md5.update(password);
-        md5.update(salt);
-        byte[] hash1 = md5.digest();
+        String key = "somerandstr12345";
+        String iv = "somerandstr12345";
 
-        md5.reset();
-        md5.update(hash1);
-        md5.update(password);
-        md5.update(salt);
-        byte[] hash2 = md5.digest();
+        System.out.println("decrypt :" + PhpEncodedString + " : " + decrypt(PhpEncodedString, key, iv));
+        System.out.println("decrypt :" + javaEncodedString + " = " + decrypt(javaEncodedString, key, iv));
 
-        md5.reset();
-        md5.update(hash2);
-        md5.update(password);
-        md5.update(salt);
-        byte[] hash3 = md5.digest();
-
-        // copy the 3 hashes in the result array
-        System.arraycopy(hash1, 0, res, 0, 16);
-        System.arraycopy(hash2, 0, res, 16, 16);
-        System.arraycopy(hash3, 0, res, 32, 16);
-        return res;
+        System.out.println("encrypt = " + new String(Base64.encodeBase64(encrypt(value, key, iv))));
     }
 
-    /*
-     * Decrypt the string data, using the password secret,
-     * in an openssl-compatible way. This function provides the equivalent
-     * of the command:
-     *
-     * echo <data> | openssl aes-256-cbc -d -base64 -pass pass:<secret>
-     */
-    public static String aesDecrypt(String secret, String data)
-            throws DigestException, NoSuchAlgorithmException,
-            NoSuchPaddingException, InvalidKeyException,
-            InvalidAlgorithmParameterException,
-            IllegalBlockSizeException,
-            BadPaddingException {
-        byte[] decoded = Base64.decodeBase64(data);
-        byte[] salt = Arrays.copyOfRange(decoded, 8, 16);
-        byte[] encrypted = Arrays.copyOfRange(decoded, 16, decoded.length);
+    public static byte[] encrypt(String  plainText, String key, String iv) throws Exception{
 
-        byte[] keyAndIV = deriveKeyAndIV(secret.getBytes(), salt);
-        byte[] key = Arrays.copyOfRange(keyAndIV, 0, 32);
-        byte[] iv = Arrays.copyOfRange(keyAndIV, 32, 48);
+        byte[] textBytes = plainText.getBytes();
+        byte[] keyBytes = key.getBytes();
+        byte[] ivBytes  = iv.getBytes();
 
-        SecretKeySpec skeySpec = new SecretKeySpec(key, "AES");
-        IvParameterSpec ivspec = new IvParameterSpec(iv);
+        SecretKeySpec secretKey=new SecretKeySpec(keyBytes,"AES");
+        IvParameterSpec ivParameterSpec=new IvParameterSpec(ivBytes);
+        Cipher cipher=Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE,secretKey,ivParameterSpec);
 
-        Cipher cipher;
-        cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.DECRYPT_MODE, skeySpec, ivspec);
-        return new String(cipher.doFinal(encrypted));
+        return cipher.doFinal(textBytes);
     }
 
-    /*
-     * Encrypt the string data, using the password secret.
-     * This function provides the equivalent of the command:
-     *
-     * echo <data> | openssl aes-256-cbc -base64 -pass pass:<secret>
-     */
-    public static String aesEncrypt(String secret, String data)
-            throws DigestException, NoSuchAlgorithmException,
-            NoSuchPaddingException, InvalidKeyException,
-            InvalidAlgorithmParameterException,
-            IllegalBlockSizeException, BadPaddingException {
-        byte [] salt = new byte [8];
-        SecureRandom sr = new SecureRandom();
-        sr.nextBytes(salt);
+    public static String decrypt(String  plainText, String key, String iv) throws Exception{
 
-        byte [] keyAndIV = deriveKeyAndIV(secret.getBytes(), salt);
-        byte [] key = Arrays.copyOfRange(keyAndIV, 0, 32);
-        byte [] iv = Arrays.copyOfRange(keyAndIV, 32, 48);
-        SecretKeySpec skeySpec = new SecretKeySpec(key, "AES");
-        IvParameterSpec ivspec = new IvParameterSpec(iv);
+        byte[] textBytes = Base64.decodeBase64(plainText);
+        byte[] keyBytes = key.getBytes();
+        byte[] ivBytes  = iv.getBytes();
 
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, skeySpec, ivspec);
-        byte [] encrypted = cipher.doFinal(data.getBytes());
+        SecretKeySpec secretKey=new SecretKeySpec(keyBytes,"AES");
+        IvParameterSpec ivParameterSpec=new IvParameterSpec(ivBytes);
+        Cipher cipher=Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE,secretKey,ivParameterSpec);
 
-        byte [] to_encode = new byte[ 16 + encrypted.length ];
-        System.arraycopy("Salted__".getBytes(), 0, to_encode, 0, 8);
-        System.arraycopy(salt, 0, to_encode, 8, 8);
-        System.arraycopy(encrypted, 0, to_encode, 16, encrypted.length);
-        return new String(Base64.encodeBase64(to_encode));
-    }
-
-    public static void main(String args[]) throws DigestException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, IOException {
-
-        // obtained with:
-        // echo -n "original string" | openssl aes-256-cbc -base64 -pass pass:secret
-        String enc = "6egwKkelFpEh0PSTvWl9tQ==";
-
-//        System.out.println(aesDecrypt("lakshay", "6egwKkelFpEh0PSTvWl9tQ=="));
-          System.out.println(aesEncrypt("lakshay", "lakshay"));
-
+        return new String(cipher.doFinal(textBytes));
     }
 
 
