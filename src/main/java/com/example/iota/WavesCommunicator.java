5c62f2da5cca1a26e3f8047e00e848a699c40dab
lakshay-saini <amit4505>
2018-12-25 17:55:47 +0530

    seedUtil minor changes

--- src/main/java/com/example/iota/WavesCommunicator.java
+++ src/main/java/com/example/iota/WavesCommunicator.java
@@ -1,11 +1,17 @@
 package com.example.iota;
 
 import com.wavesplatform.wavesj.*;
+import com.wavesplatform.wavesj.transactions.TransferTransaction;
+import com.wavesplatform.wavesj.transactions.TransferTransactionV2;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.Collection;
+import java.util.Map;
 
 public class WavesCommunicator {
+
+        private static BigDecimal WEI = new BigDecimal("100000000");
     public static void main(String[] args) throws IOException {
         String seed = "either salt enough lounge hawk labor during coin nasty enrich corn salute essay squirrel rookie";
         PrivateKeyAccount alice = PrivateKeyAccount.fromSeed(seed, 0, Account.TESTNET);
@@ -16,17 +22,18 @@ public class WavesCommunicator {
 
         // Create a Node ("https://testnode1.wavesnodes.com" by default, or you can pass another URL here)
         Node node = new Node();
-        Block block = node.getBlock(419087);
+        long balance = node.getBalance(address, Asset.WAVES);
+        System.out.println("balance = " + balance);
+
+        Block block = node.getBlock(420423);
         Collection<Transaction> transactions = block.getTransactions();
-        int height = node.getHeight();
-        System.out.println("height: " + height);
 
-        transactions.forEach(System.out::println);
-        // Get blockchain height
-//        int height = node.getHeight();
-//        System.out.println("height: " + height);
-
-        // Learn address balance
-//        System.out.println("my's balance: " + node.getBalance(address));
+        for (Transaction transaction : transactions) {
+            if(transaction.getClass().equals(TransferTransactionV2.class)) {
+                TransferTransaction transferTransaction = (TransferTransaction) transaction;
+                BigDecimal amount = BigDecimal.valueOf(transferTransaction.getAmount()).divide(WEI, 5, BigDecimal.ROUND_UP);
+                System.out.println("address : amount = " + transferTransaction.getRecipient() + ":" + amount);
+            }
+        }
     }
 }
